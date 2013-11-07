49130b504dd4d9d0fad2dd272a0bdcb0c607966a
Andreas Hägglund <gegga87@gmail.com>
2013-11-07 14:02:07 +0100

    added userId to addfriendrequest and removefriendrequest

--- src/python/quizzingbricks/web/run_web.py
+++ src/python/quizzingbricks/web/run_web.py
@@ -45,7 +45,7 @@ def add_friend():
     if request.method == 'POST':
         friend_Email = request.form['friend_email']
         if(friend_Email!= ""):
-            add_friend_response = friendservice.add_Friend(AddFriendRequest(friend_email=friend_Email))
+            add_friend_response = friendservice.add_Friend(AddFriendRequest(userId =1,friend_email=friend_Email))
             if (isinstance(add_friend_response, AddFriendResponse)):
                 #print "add response", add_friend_response
                 friends_response = friendservice.get_Friends_list(GetFriendsRequest(userId=1))  #hard coded userId
@@ -58,7 +58,7 @@ def add_friend():
             if (isinstance(friends_response,GetFriendsResponse)):
                 for friend in friends_response.friends_list:
                     friends_list=friends_list+ [friend]
-                return render_template('friends_list.html',friends_list=friends_list, error="Must fill in email of user you want to friend")   
+                return render_template('friends_list.html',friends_list=friends_list, error="Must fill in email of user you want to add")   
     else:
         friends_response = friendservice.get_Friends_list(GetFriendsRequest(userId=1))  #hard coded userId
         if (isinstance(friends_response,GetFriendsResponse)):
@@ -71,7 +71,7 @@ def remove_friend():
     friends_list = []
     if request.method == 'POST':    # removes selected friend and fetches the rest of the friendslist again if more is to be removed
         try:
-            remove_friend_response = friendservice.remove_Friend(RemoveFriendRequest(friend_email=request.form['friend']))
+            remove_friend_response = friendservice.remove_Friend(RemoveFriendRequest(userId =1,friend_email=request.form['friend']))
             if (isinstance(remove_friend_response, RemoveFriendResponse)):
                 print "remove response", remove_friend_response
                 friends_response = friendservice.get_Friends_list(GetFriendsRequest(userId=1))  #hard coded userId
@@ -93,9 +93,7 @@ def remove_friend():
     else:
         friends_response = friendservice.get_Friends_list(GetFriendsRequest(userId=1))  #hard coded userId
         if (isinstance(friends_response,GetFriendsResponse)):
-            #print friends_response
             for friend in friends_response.friends_list:
-                #print friend
                 friends_list=friends_list+ [friend]
             return render_template('friends_list.html',friends_list=friends_list)
 
