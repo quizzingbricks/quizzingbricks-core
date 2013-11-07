597d94349ae55a930d7ab69e2865c8f586eb33c5
Mikael Åhlén <mikael.ahlen@gmail.com>
2013-11-07 15:27:50 +0100

    added setup.py file to enable more correct way of install and develop the python parts

---
+++ setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+    Copyright (C) Quizzingbricks 2013
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="Quizzingbricks",
+    version="0.1-beta",
+    author="QuizzingBricks",
+    author_email="d7017e@groups.google.com", # or?
+    description="Quizzingbricks is a quiz game",
+    package_dir={"": "src/python"},
+    packages=find_packages("src/python"),
+    zip_safe=False,
+    platforms="any",
+    install_requires=[
+        "flask",
+        "sqlalchemy",
+        "alembic",
+        "psycopg2",
+        "gevent",
+        "pyzmq",
+        "redis",
+        "bcrypt",
+        "protobuf",
+        "gunicorn",
+    ]
+)
