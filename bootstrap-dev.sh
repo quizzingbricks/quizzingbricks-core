2b623e3d1cc8a067440b9c8bbf8c86a2446a03d7
Mikael Åhlén <mikael.ahlen@gmail.com>
2013-11-19 13:36:27 +0100

    updated vagrant provision file to not install docker and its requirements

--- bootstrap-dev.sh
+++ bootstrap-dev.sh
@@ -29,22 +29,22 @@
 
 # install the backported kernel
 sudo apt-get update
-sudo apt-get install linux-image-generic-lts-raring linux-headers-generic-lts-raring
+#sudo apt-get install linux-image-generic-lts-raring linux-headers-generic-lts-raring
 
 sudo apt-get install -y curl
 
 # Add the Docker repository key to your local keychain
 # using apt-key finger you can check the fingerprint matches 36A1 D786 9245 C895 0F96 6E92 D857 6A8B A88D 21E9
-sudo sh -c "curl https://get.docker.io/gpg | apt-key add -"
+#sudo sh -c "curl https://get.docker.io/gpg | apt-key add -"
 
 # Add the Docker repository to your apt sources list.
-sudo sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
+#sudo sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
 
 # Update your sources
-sudo apt-get update
+#sudo apt-get update
 
 # Install, you will see another warning that the package cannot be authenticated. Confirm install.
-sudo apt-get install -y lxc-docker
+#sudo apt-get install -y lxc-docker
 
 # build tools etc
 sudo apt-get update # TODO: remove this
