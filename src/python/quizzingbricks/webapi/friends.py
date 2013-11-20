5e7aead9c4173e4fce599318bfaca6fcb93a6428
Mikael Åhlén <mikael.ahlen@gmail.com>
2013-11-20 12:00:12 +0100

    completed friends RESTful API

--- src/python/quizzingbricks/webapi/friends.py
+++ src/python/quizzingbricks/webapi/friends.py
@@ -9,8 +9,8 @@ from quizzingbricks.webapi import app, api_error, api_errors, token_required, to
 from quizzingbricks.client.friends import FriendServiceClient
 from quizzingbricks.client.exceptions import TimeoutError
 from quizzingbricks.common.protocol import (
-    AddFriendRequest, GetFriendsRequest
-)
+    AddFriendRequest, GetFriendsRequest,
+    AddFriendResponse, RemoveFriendRequest, RemoveFriendResponse)
 import traceback
 
 friendservice = FriendServiceClient("tcp://*:5553")
@@ -31,7 +31,41 @@ def get_friends():
         return api_error("Service not available", 500), 500
 
 
-@app.route("/api/users/me/friends", methods=["POST"])
+@app.route("/api/users/me/friends/", methods=["POST"])
 @token_required
 def add_friend():
-    pass
+    try:
+        email = request.form.get("friend")
+        if not email:
+            # or a error message that tells that the email is missing?
+            return api_error("Missing required friend parameter", 004), 400
+
+        response = friendservice.add_friend(AddFriendRequest(userId=g.user.id,friend_email=email))
+        if isinstance(response, AddFriendResponse):
+            if response.friend_added:
+                return "OK"
+            return api_error("No such user exists"), 400
+        else:
+            return api_error("Internal service error", 000), 400
+
+    except Exception as e:
+        print "add_friend", e
+        traceback.print_exc()
+        return api_error("Service not available", 500), 500
+
+@app.route("/api/users/me/friends/", methods=["DELETE"])
+@token_required
+def delete_friend():
+    try:
+        friend_email = request.args.get("friend")
+        if not friend_email:
+            return api_error("Missing required friend parameter", 004), 400
+
+        response = friendservice.remove_friend(RemoveFriendRequest(userId=g.user.id, friend_email=friend_email), timeout=5000)
+        if not isinstance(response, RemoveFriendResponse):
+            return api_error("Internal service error", 500), 500 # probably return RpcError
+        return "OK" if response.friend_removed else (api_error("No such user exists", "011"), 200)
+    except Exception as e:
+        print "DELETE_FRIEND-ERROR:", e
+        traceback.print_exc()
+        return api_error("Service not available", 500), 500
