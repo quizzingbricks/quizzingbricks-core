34601bcabea94241db3f60474aa9e3dfe362f4a7
Mikael Åhlén <mikael.ahlen@gmail.com>
2013-11-30 11:23:20 +0100

    updated create lobby to adding its owner to the invites, but with the state member

--- src/python/quizzingbricks/client/lobby.py
+++ src/python/quizzingbricks/client/lobby.py
@@ -5,34 +5,34 @@
 from quizzingbricks.client import BaseClient
 
 class LobbyServiceClient(BaseClient):
-    def getLobbyId(self, request, timeout=None):
+    def get_lobby_id(self, request, timeout=None):
         with self.rpc_call("get_lobby_id", request, timeout) as response:
             return response
 
-    def getLobbyList(self, request, timeout=None):
+    def get_lobbies(self, request, timeout=None):
         with self.rpc_call("get_lobby_list", request, timeout) as response:
             return response
 
-    def getLobbyState(self, request, timeout=None):
+    def get_lobby(self, request, timeout=None):
         with self.rpc_call("get_lobby_state", request, timeout) as response:
             return response
 
-    def answerLobbyInvite(self, request, timeout=None):
+    def answer_lobby_invite(self, request, timeout=None):
         with self.rpc_call("answer_lobby_invite", request, timeout) as response:
             return response
 
-    def inviteToLobby(self, request, timeout=None):
+    def invite_to_lobby(self, request, timeout=None):
         with self.rpc_call("invite_to_lobby", request, timeout) as response:
             return response
 
-    def removeLobby(self, request, timeout=None):
+    def remove_lobby(self, request, timeout=None):
         with self.rpc_call("remove_lobby", request, timeout) as response:
             return response
    
-    def startGame(self, request, timeout=None):
+    def start_game(self, request, timeout=None):
         with self.rpc_call("start_game", request, timeout) as response:
             return response
             
-    def createLobby(self, request, timeout=None):
+    def create_lobby(self, request, timeout=None):
         with self.rpc_call("create_lobby", request, timeout) as response:
             return response
